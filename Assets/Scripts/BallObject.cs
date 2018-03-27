18727bbbae2bb33b38dfde4c1193eb1375459009
Florian Vidal <florianvidals@gmail.com>
2018-03-27 22:10:57 +0200

    Déplacement de la poubelle dans les différents niveaux

--- Assets/Scripts/BallObject.cs
+++ Assets/Scripts/BallObject.cs
@@ -1,23 +1,62 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BallObject : MonoBehaviour {
+
+    const float X_MIN = 0.18f;
+    const float X_MAX = 1.4f;
+    const float Z_MIN = -1.9f;
+    const float Z_MAX = 0f;
+    const float Y = 0.031f;
+
     [SerializeField] TextMesh scoreText;
+    [SerializeField] bool moveMode;
+    [SerializeField] bool timeMode;
+    [SerializeField] bool randomTime;
+    [SerializeField] float timeMin;
+    [SerializeField] float timeMax;
+
     private AudioSource[] sons;
     private SphereCollider trashCollider;
     private Vector3 origin;
     private int score;
 
-	// Use this for initialization
-	void Start () {
+    private float deltaTime;
+    private float timer;
+    private bool started;
+
+    // Use this for initialization
+    void Start () {
         sons = GetComponents<AudioSource>();
         trashCollider = GameObject.Find("Trash").GetComponent<SphereCollider>();
         origin = transform.position;
         score = 0;
         this.GetComponent<Rigidbody>().isKinematic = true;
         this.GetComponent<Rigidbody>().useGravity = false;
+
+        deltaTime = (randomTime) ? randomFloat(timeMin, timeMax) : 2f;
+        timer = 0;
+    }
+
+    void Update()
+    {
+        if (timeMode)
+        {
+            if(origin != transform.position) {
+                started = true;
+            }
+            if (started)
+            {
+                timer += Time.deltaTime;
+                if (timer > deltaTime)
+                {
+                    setTrashPosition();
+                }
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -33,6 +72,36 @@ public class BallObject : MonoBehaviour {
             this.GetComponent<Rigidbody>().velocity = Vector3.zero;
             score++;
             scoreText.text = "Score : " + score;
+
+            if (moveMode)
+            {
+                setTrashPosition();
+            }
         }
     }
+
+    /**
+     * Set the position of the trash
+     */
+    private void setTrashPosition()
+    {
+        float x = randomFloat(X_MIN, X_MAX);
+        float z = randomFloat(Z_MIN, Z_MAX);
+        GameObject.Find("Trash").transform.position = new Vector3(x, Y, z);
+        if (randomTime)
+        {
+            deltaTime = randomFloat(timeMin, timeMax);
+        }
+        timer = 0;
+
+    }
+
+    private float randomFloat(float min, float max)
+    {
+        System.Random random = new System.Random(System.DateTime.Now.TimeOfDay.Milliseconds);
+        var result = (random.NextDouble()* (max - (double)min))+ min;
+        return (float)result;
+    }
+
+
 }
